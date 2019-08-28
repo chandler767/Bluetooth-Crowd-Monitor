ed6b752f82ef3193153fb02583c44faacd9fc8aa
Chandler Mayo <chandler@chandlermayo.com>
2019-08-28 15:33:32 -0400

    Added serverless function to count advertising BLE devices and publish current count.

---
+++ functions/advertising_devices_counter.js
@@ -0,0 +1,32 @@
+// Counts the number of advertising BLE devices.
+const store = require('kvstore');
+const pubnub = require('pubnub');
+
+export default (request) => {
+    // Get current count.
+    return store.get('advertising_devices').then((value) => {
+        if (request.message == "Scan Ended") {
+            // Publish new count.
+            pubnub.publish({
+                "channel": "advertised_devices_count",
+                "message": value.count
+            }).then((publishResponse) => {
+                console.log(`Publish Status: ${publishResponse[0]}:${publishResponse[1]} with TT ${publishResponse[2]}`);
+            });
+            // Reset count.
+            store.set('advertising_devices', {
+                count: 0
+            });
+        } else {
+            // Add device to count.
+            store.set('advertising_devices', {
+                count: value.count+1
+            });
+        }
+        return request.ok();
+    })
+    .catch((e) => {
+        console.error(e);
+    });
+    return request.ok();
+}
